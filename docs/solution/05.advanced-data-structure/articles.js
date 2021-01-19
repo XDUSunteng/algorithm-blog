c5af32f9f3dbab5631b2b1e186f031a229242c2a
SunTeng <teng_sun@qq.com>
2021-01-20 00:56:05 +0800

    添加第5章题解

--- docs/solution/05.advanced-data-structure/articles.js
+++ docs/solution/05.advanced-data-structure/articles.js
@@ -2,17 +2,17 @@ module.exports = {
     getAdvancedDataStructureSidebar: () => [{
         title: '并查集',
         children: [
-
+            '/solution/05.advanced-data-structure/5.1 并查集/题解-食物链-并查集.md',
         ],
     }, {
         title: '树状数组',
         children: [
-
+            '/solution/05.advanced-data-structure/5.2 树状数组/5.2.2 权值树状数组/题解-逆序对-权值树状数组.md',
         ],
     }, {
         title: '线段树',
         children: [
-
+            '/solution/05.advanced-data-structure/5.3 线段树/_普通_线段树/题解-维护序列-线段树.md',
         ],
     }, {
         title: '分块',
@@ -27,7 +27,7 @@ module.exports = {
     }, {
         title: '二叉查找树与平衡树初步',
         children: [
-
+            '/solution/05.advanced-data-structure/5.6 二叉查找树与平衡树初步/题解-邻值查找-平衡树.md',
         ],
     }, {
         title: '离线分治算法',
